c3ffce812ce8238544f2697dee65a6f8ac35c1dc
Anmar Jarjees <anmarjarjees@users.noreply.github.com>
2025-09-16 18:48:52 +0000

    any message you like :-)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cpp-assignment01-group01
 Assignment1 about the basic of using C++ language.
+
+# Group Members:
+- Alex Chow
+- Sam Simpson
+- Sali Gradson
+
+## Course Name:
+Or course is about Arduino and C++ coding.
+
+### Program Name:
+Our program is CSTN (Computer System Tech)
+
+#### The hotkey for reviewing the readme file => CTRL + SHIFT + V 
---
+++ assignment1.cpp
@@ -0,0 +1,12 @@
+
+// start my assignment1.cpp:
+#include <iostream>
+// iostream use for input and output operations
+int main() {
+    std::cout << "Assignment 1 Demo" << std::endl;
+    // following the assignment instructions
+    // resume my assignment1.cpp: etc...
+    
+    // always return 0 to indicate that the program ended successfully
+    return 0;
+}
